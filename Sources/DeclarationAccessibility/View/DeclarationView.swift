e896378434eb56240e65c98a275f0d0fe7300cd3
Tayeb Sedraia <tayeb.sedraia@orange.com>
2023-10-02 15:53:48 +0200

    add mit license

--- Sources/DeclarationAccessibility/View/DeclarationView.swift
+++ Sources/DeclarationAccessibility/View/DeclarationView.swift
@@ -1,16 +1,40 @@
 //
-//  SwiftUIView.swift
+// MIT License
+// Copyright (c) 2023 Orange
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the  Software), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 //
-//  Created by Orange on 21/09/2023.
+// THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 //
 
 import SwiftUI
 
 public struct DeclarationView: View {
+    
+    // =======================
+    // MARK: Properties
+    // =======================
     var declarations: Declaration
     var selectedTheme: Theme
 
+    
+    // =================
+    // MARK: Initializer
+    // =================
+    
     public init(xmlFileName: String, selectedTheme: Theme, url: String) {
         let parser = DeclarationDataParser()
         parser.parseXML(fileName: xmlFileName)
